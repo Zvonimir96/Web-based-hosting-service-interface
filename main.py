5249bc7ddc0eb4c54e6fdc0fd0a361180e392c67
Zvonimir <zlavric@tvz.hr>
2023-07-27 19:52:58 +0200

    Main method added.

--- main.py
+++ main.py
@@ -4,35 +4,41 @@ from credentials import TOKEN
 language = 'Python'
 username = 'Zvonimir96'
 
-gh = GitHub(username, token=TOKEN)
 
-# Retrieve all repository's
-print('All repos', end=': ')
-print(*gh.repos(), sep=', ')
+def main():
+    gh = GitHub(username, token=TOKEN)
 
-# Retrieve all repository's with a desired language in descending order
-print(f'Only {language} repos', end=': ')
-print(*gh.repos(language, direction='desc'), sep=", ")
+    # Retrieve all repository's
+    print('All repos', end=': ')
+    print(*gh.repos(), sep=', ')
 
-# Retrieve repository by name
-repo = gh.repo("NeoPixel-strip")
+    # Retrieve all repository's with a desired language in descending order
+    print(f'Only {language} repos', end=': ')
+    print(*gh.repos(language, direction='desc'), sep=", ")
 
-# Retrieve all commits from NeoPixel-strip repository
-print(f'{repo} commits: ')
-print(*gh.commits(repo), sep="\n")
+    # Retrieve repository by name
+    repo = gh.repo("NeoPixel-strip")
 
-# Retrieve all languages from NeoPixel-strip repository
-print(f'{repo} languages', end=': ')
-print(*gh.languages(repo), sep=", ")
+    # Retrieve all commits from NeoPixel-strip repository
+    print(f'{repo} commits: ')
+    print(*gh.commits(repo), sep="\n")
 
-# Print number of request that can be sent to Github API from this interface
-gh.rate_limit()
+    # Retrieve all languages from NeoPixel-strip repository
+    print(f'{repo} languages', end=': ')
+    print(*gh.languages(repo), sep=", ")
 
-# Retrieve authorized user information
-print(gh.user())
+    # Print number of request that can be sent to Github API from this interface
+    gh.rate_limit()
 
-# Retrieve all unprotected branches from NeoPixel-strip repository
-print(*gh.branches(repo, protected='false'), sep=', ')
+    # Retrieve authorized user information
+    print(gh.user())
 
-# Retrieve main branch from NeoPixel-strip
-print(gh.branch(repo, 'main'))
+    # Retrieve all unprotected branches from NeoPixel-strip repository
+    print(*gh.branches(repo, protected='false'), sep=', ')
+
+    # Retrieve main branch from NeoPixel-strip
+    print(gh.branch(repo, 'main'))
+
+
+if __name__ == "__main__":
+    main()
